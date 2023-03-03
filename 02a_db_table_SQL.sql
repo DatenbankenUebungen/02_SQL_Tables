bc2dc16afde62046c79e9aca9fb4dcb63e1634af
Rekxis <rekxis@gmx.de>
2023-03-03 11:53:09 +0000

    added new syntax, filling tables in database

--- 02a_db_table_SQL.sql
+++ 02a_db_table_SQL.sql
@@ -16,3 +16,50 @@ SHOW DATABASES;
 CREATE DATABASE IF NOT EXISTS boo;
 
 SHOW DATABASES;
+
+# DB auswählen
+USE boo;
+
+# Tabellen der ausgewählten DB anzeigen
+SHOW TABLES;
+
+# Lösche Tabelle, unnötig da wir die DB vorher löschen!
+DROP TABLE IF EXISTS coworkers;
+
+# Erstelle Tabelle in der aktuellen DB
+CREATE TABLE IF NOT EXISTS coworkers
+(
+    firstName VARCHAR(20),
+    location VARCHAR(20),
+    age INT,
+    computer VARCHAR(20)
+);
+
+SHOW TABLES;
+
+# Befülle die Tabelle mit Werten
+INSERT INTO coworkers (firstName, location, age, computer)
+VALUES
+    ("Markus", "coffin", "150", "Smartphone"),
+    ("Juergen", "home", "25", "PC"),
+    ("Hans", "office", "35", "PC"),
+    ("Peter", "pool", "40", "Tablet"),
+    ("Bama", "airport", "52", "Notebook");
+
+SELECT * FROM coworkers;
+
+INSERT INTO coworkers (firstName, location, age, computer)
+VALUES
+    ("Max", "office", 35, "PC"),
+    ("Gani", "pool", 36, "iPad"),
+    ("Sohrab", "bed", 26, "Mac");
+    
+# Zeige alle Inhalte der Tabelle coworkers an
+SELECT * FROM coworkers;
+
+# Zeigt die Beschreibung der Tabelle coworkers an
+DESCRIBE coworkers;
+
+# Sortiere die Tabelle coworkers nach den Werten in age
+SELECT * FROM coworkers
+ORDER BY age;
