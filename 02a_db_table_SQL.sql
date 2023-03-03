fc23da21237966bea31c5de6d0f44b9c7d5be41e
Rekxis <rekxis@gmx.de>
2023-03-03 10:49:30 +0000

    added comments

--- 02a_db_table_SQL.sql
+++ 02a_db_table_SQL.sql
@@ -4,13 +4,15 @@
 #  Kommentar 2
 -- Kommentar 3
 
-/* Datenbanken auf Server anzeigen */
+# Datenbanken auf Server anzeigen
 SHOW DATABASES;
 
+# DB boo löschen, falls vorhanden
 DROP DATABASE IF EXISTS boo;
 
 SHOW DATABASES;
 
+# DB boo anlegen, falls noch NICHT vorhanden
 CREATE DATABASE IF NOT EXISTS boo;
 
 SHOW DATABASES;
